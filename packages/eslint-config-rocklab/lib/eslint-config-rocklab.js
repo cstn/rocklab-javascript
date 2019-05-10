990cf42458c0b953636546b650baea010e77faa4
Carsten Stein <carsten.stein@cast-it.de>
2019-05-10 15:06:06 +0200

    feature/react (#4)
    * Add react rules
    
    * v1.0.0

--- packages/eslint-config-rocklab/lib/eslint-config-rocklab.js
+++ packages/eslint-config-rocklab/lib/eslint-config-rocklab.js
@@ -1,5 +1,11 @@
 module.exports = {
-  extends: ['eslint-config-airbnb-base'],
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'testcafe'],
+  extends: [
+    'eslint-config-airbnb-base',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:testcafe/recommended',
+  ],
   rules: {
     'arrow-parens': 0,
     'arrow-spacing': 'error',
@@ -21,5 +27,43 @@ module.exports = {
     'prefer-const': 'error',
     'prefer-template': 'error',
     radix: ['warn', 'always'],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/boolean-prop-naming': ['error', { rule: '^(is|has)[A-Z]([A-Za-z0-9]?)+' }],
+    'react/button-has-type': ['error'],
+    'react/destructuring-assignment': [1, 'always'],
+    'react/no-array-index-key': ['error'],
+    'react/no-unused-prop-types': 1,
+    'react/no-unused-state': 1,
+    'react/prefer-stateless-function': 2,
+    'react/jsx-closing-bracket-location': 1,
+    'react/jsx-no-bind': 1,
+    'react/jsx-sort-props': [
+      1,
+      {
+        callbacksLast: true,
+        ignoreCase: true,
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+  },
+  env: {
+    amd: true,
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: '16',
+    },
   },
 };
