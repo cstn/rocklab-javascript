dfd7c332278979850deaa7cd10416849dcca62cc
Carsten Stein <carsten.stein@cast-it.de>
2022-01-05 08:34:18 +0100

    Fix prettier

--- packages/eslint-config-typescript/lib/eslint-config-base.js
+++ packages/eslint-config-typescript/lib/eslint-config-base.js
@@ -1,9 +1,9 @@
 module.exports = {
   extends: [
-    'eslint-config-airbnb-typescript/base',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     '@rocklab/eslint-config-javascript/base',
+    'eslint-config-airbnb-typescript/base',
     './lib/rules/stylistic-issues.js',
   ],
   rules: {},
