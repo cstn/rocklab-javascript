5ed72c369e2e5e27d3853ab389fd4ef41d762003
Carsten Stein <carsten.stein@cast-it.de>
2022-01-11 18:47:32 +0100

    feat: add changelog

--- packages/commitlint-config/lib/commitlint-config.js
+++ packages/commitlint-config/lib/commitlint-config.js
@@ -4,8 +4,6 @@ const Configuration = {
   ],
   rules: {
     'footer-max-length': [2, 'always', 72],
-    'subject-case': [2, 'always', 'sentence-case'],
-    'type-enum': [2, 'always', ['chore', 'feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert']],
   },
 };
 
